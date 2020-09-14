4e997e74f9b6aeb735a0765a6289af5e4cab80bf
gunnerVivek <vivpur88@gmail.com>
2020-09-14 12:24:28 +0530

    refactor: changed directory name for tarnsformed data

--- definitions.py
+++ definitions.py
@@ -4,5 +4,4 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DATA_DIR = os.path.join(ROOT_DIR, 'data')
 
-TRANSFORMED_DATA_DIR = os.path.join(ROOT_DIR, 'processed data')
-
+TRANSFORMED_DATA_DIR = os.path.join(ROOT_DIR, 'transformed_data')
