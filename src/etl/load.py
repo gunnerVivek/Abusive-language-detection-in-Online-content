3269a2ef8539d43a136e143c2fbc1430c39728f7
gunnerVivek <vivpur88@gmail.com>
2020-09-15 23:26:46 +0530

    'etl: Add code to load transformed data into DB'

--- src/etl/load.py
+++ src/etl/load.py
@@ -1,12 +1,19 @@
 
 import os
+import sys
+
 import pathlib
+
 from pandas import read_csv
 
 import mysql.connector as connector
 
+import mysql.connector.errors as mysql_error
+import traceback
+
 from definitions import TRANSFORMED_DATA_DIR
-from configurations import GCP_TRANSLATE_CREDENTIALS_PATH
+
+from configurations import TRANSFORMED_DATA_DB_CONFIG
 
 
 # facebook_hate_speech_translated.csv
@@ -27,81 +34,196 @@ from configurations import GCP_TRANSLATE_CREDENTIALS_PATH
 
 class LoadTransformed:
     '''
-        Methods to load all oof the Transformed data into DB
+        Methods to load all of the Transformed data into DB
     '''
 
-    @staticmethod
-    def __setup_gcp_credentials():
-        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = GCP_TRANSLATE_CREDENTIALS_PATH
+    def __setup_db_connection(self, **config):
+        '''
+            Setup the connection to the DB
+        '''
+        
+        try:
+            self.connection = connector.connect(**config)
+            self.cursor = self.connection.cursor(buffered=True)
+        
+        except mysql_error.InterfaceError as e: # data base did not respond (service down or otherwise) | # wrong port | 3 wrong host IP
+            print(traceback.format_exc) #Log
+            raise # will be caught down the line
+            # TODO: Catch the exceptions, log and rethrow to be caught by
+        
+        except  mysql_error.ProgrammingError as e: # unknown database | # acess denied wrong password | # wrong user
+            print(traceback.format_exc) #Log
+            raise # will be caught down the line
+
+        except Exception as e: # unknown exception
+            print(traceback.format_exc) #Log
+            raise # will be caught down the lin
+
+    
+    def __close_db_connection(self):
+        '''
+            close the connection.
+        '''
+        try:
+            if self.connection.is_connected():
+                    self.cursor.close()
+                    self.connection.close()
+
+        except NameError as e: #happens when connection/cursor is not defined. May be if called after error.
+            traceback.format_exc()
+
+        except  Exception as e: # unknown exception
+            traceback.format_exc()
 
 
-
-    def __create_table(self, table_name, cursor):
+    def __create_table(self, table_name):
         '''
             creates a Single table.
 
-            try Except block not used 
+            if exception occured, will return the exception
         '''
 
-        drop_query = "DROP TABLE IF EXISTS {table_name};".format(table_name=table_name)
+        try:
+            drop_query = "DROP TABLE IF EXISTS {table_name};".format(table_name=table_name)
 
-        create_query = '''
-                            CREATE TABLE {table_name}(
-                                    message TEXT,
-                                    label VARCHAR(10)
-                            );
-                    '''.format(table_name=table_name)
+            create_query = '''
+                                CREATE TABLE {table_name}(
+                                        message TEXT,
+                                        label VARCHAR(10)
+                                );
+                        '''.format(table_name=table_name)
 
-        cursor.execute(drop_query)
-        cursor.execute(create_query)
-
-
-    def __fetch_csv(self, parameter_list):
-        pass
-    
-    def __write_to_db(self, dataset, table_name=None, contains_header=False):
+            self.cursor.execute(drop_query)
+            self.cursor.execute(create_query)
+        except mysql_error.ProgrammingError as e: # TABLE ALREADY EXISTS - for CREATE query
+            print(traceback.format_exc())
+            raise # pass down the line
         
-        self.__create_table(table_name, cursor)
-        
+        # not needed DROP TABLE already checks for IF EXISTS
+        except mysql_error.DatabaseError as e: # Unknown table
+            print('table `{}` does not exist'.format(table_name))
+            print(traceback.format_exc())
+            raise # pass down the line
+
+        except Exception as e:
+            print(traceback.format_exc()) # Log
+            raise # pass down the line
+
+
+
+    def __write_to_db(self, dataset, table_name=None):
+        '''
+            Load data into DB Table
+        '''
         # LOAD DATA LOCAL INFILE --> disabled (specifically LOCAL )in MySQL 8
         # by default due to potential security issue.
+        try:
+            # preps for fast repeated execution
+            prepare_stmt = " PREPARE stmt FROM 'INSERT INTO {table_name} (`message`,`label`) VALUES(?,?)'; "
+            self.cursor.execute(prepare_stmt)
 
-        # preps for fast repeated execution
-        prepare_stmt = ''' PREPARE stmt FROM 'INSERT INTO {table_name} (`message`,`label`) VALUES(?,?)'; '''
-        cursor.execute(prepare_stmt)
+            insert_stmt  = " EXECUTE stmt USING {message}, {label} "
 
-        for index, row in dataset.iterrows():
+            for index, row in dataset.iterrows():
 
-            insert_stmt  = " EXECUTE stmt USING {message}, {label} ".format(message=row['message'], label=row['label'])
-            cursor.execute(insert_stmt)
-        
-        deallocate_stmt = "DEALLOCATE PREPARE stmt;"
-        cursor.execute(deallocate_stmt)
+                self.cursor.execute(insert_stmt.format(message=row['message'], label=row['label']))
+            
+            # print('Loading of {table_name} completed'.format(table_name=table_name)) # Log
 
-    def pipeline(self, parameter_list):
+            deallocate_stmt = "DEALLOCATE PREPARE stmt;"
+            self.cursor.execute(deallocate_stmt)
+
+        except mysql_error.DatabaseError as e: # Unknown table - table does not exists
+            print('table `{}` does not exist'.format(table_name))
+            print(traceback.format_exc())
+            raise # pass down the line
+
+        except expression as identifier:
+            print(traceback.format_exc())
+            raise # pass down the line
+
+
+
+    def pipeline(self, data_path_list, files_have_header=True):
         
         '''
             Will be called once for each transformed data source.
+            Expects an iterable of data sources.
         '''
-        # create table
-        # fetch
-        # write_to_db
 
-        for data in data_list:
+        try:
+            self.__setup_db_connection(**TRANSFORMED_DATA_DB_CONFIG)
+        except Exception:
+            print('Failed to Establissh connection with the Database')
+            print(traceback.format_exc())
+            sys.exit(1)
+
+        # iterate through each source
+        for path in data_path_list:
+
+            # c:/repositories/abc.csv --> abc
+            table_name = pathlib.Path(path).stem
+            
+            ## 1. fetch data from file
             try:
-                # re assign column names for uniformity
-                pd.read_csv(data, header=0, names=['message', 'label'])
-            except expression as identifier:
-                # if access denied exception stop
-                # else continue to next data set
+                if files_have_header:
+
+                    # first row is expected to be header and will be skipped
+                    # re assign column names for uniformity
+                    data = read_csv(path, header=0, names=['message', 'label'])
+                else:
+                    # assign column names
+                    data = read_csv(path, names=['message', 'label'])
+            
+            except FileNotFoundError as e:
+                print('{} not found.'.format(path))
+                print(traceback.print_exc()) # Log
+                continue # next data source
+
+            except IOError as e:
+                # permissin denied and other IO exceptions
+                print(traceback.print_exc()) # Log
                 continue
+            
+            except Exception as e:
+                # unknown exception
+                print(traceback.print_exc()) # Log
+                continue
+            
+            ## 2. Create Table and 3. Insert into table
+            try:
+                ## 2. create table
+                self.__create_table(table_name)
+                
+                ## 3. write_to_db
+                self.__write_to_db(dataset=data, table_name=table_name)
+
+                print('data insert successfull for {}'.format(table_name)) # Log
+
+            except Exception as e:
+                # Catching exception thrown from called function
+                print(traceback.format_exec())  # Log
+                continue
+            
 
 
+        # close the connection
+        try:
+            self.__close_db_connection()
+        except Exception as e:
+            print('Failed to close conection properly')
 
 
-data = read_csv(os.path.join(TRANSFORMED_DATA_DIR, name), squeeze=True, usecols=[0])
-print(data.shape)
+if __name__ == "__main__":
+    
+    # use pathlib to get all files .csv
+    file_pattern = '*.csv'
+    file_paths = list(pathlib.Path(TRANSFORMED_DATA_DIR).glob(file_pattern))
 
-data = read_csv(os.path.join(TRANSFORMED_DATA_DIR, name), squeeze=True, nrows=5)
-print(data)
+    load_transformed = LoadTransformed()
 
+    # call the data sources
+    load_transformed.pipeline(file_paths)
+
+    # clean up
+    del load_transformed
