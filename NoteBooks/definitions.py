1f1614c8e8bdc8225dc31cdb987eb456b27e391f
gunnerVivek <vivpur88@gmail.com>
2020-09-25 21:22:56 +0530

    chore: Adds needed definations at directory level

---
+++ NoteBooks/definitions.py
@@ -0,0 +1,17 @@
+'''
+	THis module contains all the definations and configurations needed for the etl package.
+'''
+
+import pathlib
+import os.path
+
+ROOT_DIR = pathlib.Path('root.py').resolve().parent
+
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
+
+TRANSFORMED_DATA_DIR = os.path.join(ROOT_DIR, 'transformed_data')
+
+CLEANED_DATA_DIR = os.path.join(ROOT_DIR, 'cleaned_data')
+
+ABUSE = 'abuse'
+NO_ABUSE = 'no_abuse'
